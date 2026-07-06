64f66594b5b7f9ec98a32edbfba4c5230a34ddea
Ankit Ydv <ankityadav11241@gmail.com>
2026-07-07 00:11:12 +0530

    autograd engine with addition & multiplication support

--- 04-autograd-engine/autograd.py
+++ 04-autograd-engine/autograd.py
@@ -1,20 +1,49 @@
 class Value:
-    def __init__(self, value, children = (), op = ""):
+    def __init__(self, value, _prev = (), op = ""):
         self.value = value
         self.grad = 0
-        self.children = children
+        self._prev = _prev
         self.op = op
+        self._backward = lambda: None
 
     def __repr__(self):
-        return f"Value(value={self.value}, grad={self.grad}, children={self.children}, op={self.op})"
+        return f"Value(value={self.value}, grad={self.grad}, _prev={self._prev}, op={self.op})"
     
     def __add__(self, other):
-        print("adding", self.value, other.value)
-        return Value(value=self.value + other.value, children=(self, other), op="+")
+        out = Value(value=self.value + other.value, _prev=(self, other), op="+")
+        def _backward():
+            self.grad += out.grad
+            other.grad += out.grad
+        out._backward = _backward
+        return out
 
     def __mul__(self, other):
-        print("multiplication", self.value, other.value)
-        return Value(value=self.value * other.value, children=(self, other), op="*")
+        out = Value(value=self.value * other.value, _prev=(self, other), op="*")
+        def _backward():
+            self.grad += out.grad * other.value
+            other.grad += out.grad * self.value
+        out._backward = _backward
+        return out
+
+    def build(self):
+        visited: set[Value] = set()
+        topo: list[Value] = list()
+        def buildGraph(self: Value):
+            if self in visited:
+                return
+            visited.add(self)
+            if self._prev:
+                for ch in self._prev:
+                    buildGraph(ch)
+            topo.append(self)
+        buildGraph(self)
+        return topo
+
+    def backward(self):
+        topo = self.build()
+        self.grad = 1
+        for node in reversed(topo):
+            node._backward()
 
 if __name__ == "__main__":
     a = Value(2)
@@ -28,3 +57,9 @@ if __name__ == "__main__":
 
     d = c * Value(8)
     print(d)
+    print("="*100)
+    d.backward()
+    print("a.grad=", a.grad)
+    print("b.grad=", b.grad)
+    print("c.grad=", c.grad)
+    print("d.grad=", d.grad)
