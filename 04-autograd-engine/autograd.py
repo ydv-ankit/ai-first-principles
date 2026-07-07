d0317b30729950a66827a3924bb08b62c79020e6
Ankit Ydv <ankityadav11241@gmail.com>
2026-07-07 22:47:07 +0530

    Enhanced autograd engine with support for additional operations: subtraction, power, and division. Improved handling of non-Value types in addition and multiplication. Added reverse operations and negation.

--- 04-autograd-engine/autograd.py
+++ 04-autograd-engine/autograd.py
@@ -8,22 +8,6 @@ class Value:
 
     def __repr__(self):
         return f"Value(value={self.value}, grad={self.grad}, _prev={self._prev}, op={self.op})"
-    
-    def __add__(self, other):
-        out = Value(value=self.value + other.value, _prev=(self, other), op="+")
-        def _backward():
-            self.grad += out.grad
-            other.grad += out.grad
-        out._backward = _backward
-        return out
-
-    def __mul__(self, other):
-        out = Value(value=self.value * other.value, _prev=(self, other), op="*")
-        def _backward():
-            self.grad += out.grad * other.value
-            other.grad += out.grad * self.value
-        out._backward = _backward
-        return out
 
     def build(self):
         visited: set[Value] = set()
@@ -45,6 +29,65 @@ class Value:
         for node in reversed(topo):
             node._backward()
 
+    def __add__(self, other):
+        if not isinstance(other, Value):
+            other = Value(other)
+
+        out = Value(self.value + other.value, (self, other), "+")
+
+        def _backward():
+            self.grad += out.grad
+            other.grad += out.grad
+
+        out._backward = _backward
+        return out
+    
+    def __mul__(self, other):
+        if not isinstance(other, Value):
+            other = Value(other)
+
+        out = Value(self.value * other.value, (self, other), "+")
+
+        def _backward():
+            self.grad += out.grad * other.value
+            other.grad += out.grad * self.value
+
+        out._backward = _backward
+        return out
+    
+    def __radd__(self, other):
+        return self + other
+    
+    def __rmul__(self, other):
+        return self * other
+
+    def __neg__(self):
+        return self * -1
+    
+    def __sub__(self, other):
+        return self + (-other)
+
+    def __pow__(self, other):
+        assert isinstance(other, (int, float))
+
+        out = Value(self.value ** other, (self,), f"**{other}")
+
+        def _backward():
+            self.grad += out.grad * other * (self.value ** (other - 1))
+
+        out._backward = _backward
+
+        return out
+    
+    def __truediv__(self, other):
+        if not isinstance(other, Value):
+            other = Value(other)
+
+        return self * (other ** -1)
+
+    def __rtruediv__(self, other):
+        return other * (self ** -1)
+
 if __name__ == "__main__":
     a = Value(2)
     b = Value(3)
@@ -55,7 +98,7 @@ if __name__ == "__main__":
     c = a + b
     print(c)
 
-    d = c * Value(8)
+    d = c * a
     print(d)
     print("="*100)
     d.backward()
